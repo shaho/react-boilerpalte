29deac35b141ed578ff33a29b70d7b558e3ef291
shaho <toofani@gmail.com>
2019-06-09 18:56:29 +0430

    initial version

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "create-react-app 🖕";
+
+ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
+
+module.hot.accept();
